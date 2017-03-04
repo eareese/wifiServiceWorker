f0a32a716b5e3a5d06dae0ddee8d82752182b29b
eareese <eareese@eareese.com>
2017-03-04 17:40:25 -0500

    Cleanup syntax

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js', { scope: '.' }).then(function(reg) {
-    console.log('Registered service worker ', reg);
-  });
+  navigator.serviceWorker.register('/sw.js', { scope: '.' })
+    .then(function(reg) {
+      console.log('Registered service worker ', reg);
+    });
 
   navigator.serviceWorker.addEventListener('controllerchange', function(event) {
     console.log('[controllerchange] A "controllerchange" event has happened ' +
@@ -12,7 +13,7 @@ if ('serviceWorker' in navigator) {
       console.log('[controllerchange][statechange] ' , this.state);
 
       if (this.state === 'activated') {
-        console.log('ready to go offline!')
+        console.log('ready to go offline!');
       }
     });
   });
